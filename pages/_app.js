cd098309d5a84a9008a74b2902d344038ed91a1d
n0step_ <krushnavyas06@gmail.com>
2024-10-09 18:56:20 +0000

    fix: head -> title

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import 'font-awesome/css/font-awesome.min.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Links Manager</title>
+        <meta name="description" content="Description of my website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
