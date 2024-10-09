fc634cd14bef9d0ead37eb9e2883d72344b72f66
n0step_ <krushnavyas06@gmail.com>
2024-10-09 18:06:07 +0000

    feat: login form & links page.

---
+++ pages/links.js
@@ -0,0 +1,11 @@
+const LinksPage = () => {
+    return (
+      <div>
+        <h1>Links Page</h1>
+        <p>This page will contain the links.</p>
+      </div>
+    );
+  };
+  
+  export default LinksPage;
+  
