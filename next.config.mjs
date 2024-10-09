ec4a3abf97509144046aadb7f3cf84a1a0e840e9
n0step_ <krushnavyas06@gmail.com>
2024-10-09 18:18:47 +0000

    add: expose environment variables

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_USERNAME: process.env.USERNAME,
+    NEXT_PUBLIC_PASSWORD: process.env.PASSWORD,
+  },
   reactStrictMode: true,
 };
 
